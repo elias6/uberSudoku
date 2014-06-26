50ad257e588f4159c358230f9bf31e3eebf7d912
Elias Zamaria <mikez302@gmail.com>
2014-06-26 12:47:33 -0700

    Move Sudoku code to a jQuery plugin

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-    $(".uberSudoku").each(function () {
-        var $grid = $("<table class='grid' />").appendTo(this);
-        _(9).times(function () {
-            $row = $("<tr />").appendTo($grid);
+    $(".uberSudoku").uberSudoku();
+});
+
+(function ($, undefined) {
+    $.fn.uberSudoku = function (options) {
+        this.each(function () {
+            var $grid = $("<table class='grid' />").appendTo(this);
+
             _(9).times(function () {
-                $cell = $("<td />").appendTo($row);
+                $row = $("<tr />").appendTo($grid);
+                _(9).times(function () {
+                    $cell = $("<td />").appendTo($row);
+                });
+            });
+
+            $grid.find("td:not(.given)").each(function () {
+                $(this).append("<input type='text' maxlength='1' />");
             });
         });
 
-        $grid.find("td:not(.given)").each(function () {
-            $(this).append("<input type='text' maxlength='1' />");
-        });
-    });
-});
+        return this;
+    };
+})(jQuery);
