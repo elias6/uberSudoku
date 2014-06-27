478375207a433044c4eacb73cd2fd0fb4b1da7e6
Elias Zamaria <mikez302@gmail.com>
2014-06-26 23:14:57 -0700

    Added methods to get row, column, and box of a cell

--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,32 @@ $(document).ready(function () {
 
 (function ($, undefined) {
     $.fn.uberSudoku = function (options) {
+        function getRow(cell) {
+            var $cell = $(cell).closest("td");
+            return $cell.closest("tr").find("td");
+        }
+
+        function getColumn(cell) {
+            var $cell = $(cell).closest("td"),
+                index = $cell.index();
+            return $cell.closest(".grid").find("td").filter(function () {
+                return $(this).index() === index;
+            });
+        }
+
+        function getBox(cell) {
+            var $cell = $(cell).closest("td"),
+                rowIndex = $cell.closest("tr").index(),
+                columnIndex = $cell.index(),
+                band = [[0, 1, 2], [3, 4, 5], [6, 7, 8]][Math.floor(rowIndex / 3)],
+                stack = [[0, 1, 2], [3, 4, 5], [6, 7, 8]][Math.floor(columnIndex / 3)];
+            return $cell.closest(".grid").find("tr").filter(function () {
+                return band.indexOf($(this).index()) !== -1;
+            }).find("td").filter(function () {
+                return stack.indexOf($(this).index()) !== -1;
+            });
+        }
+
         this.each(function () {
             $(this).addClass("uberSudoku");
 
