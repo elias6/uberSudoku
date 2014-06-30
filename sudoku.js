21ada4dbf541e4c009e6b4d4f8963f6db49be47a
Elias Zamaria <mikez302@gmail.com>
2014-06-30 11:09:12 -0700

    Give "cell" class to cells
    This is for readability, and maybe avoiding ambiguity if table cells
    are ever used for anything else.

--- sudoku.js
+++ sudoku.js
@@ -15,11 +15,11 @@ $(document).ready(function () {
             this.$grid = this.createGrid();
 
             this.rows = this.$grid.find("tr").map(function (i, row) {
-                return $(row).find("td");
+                return $(row).find(".cell");
             }).get();
 
             this.columns = _(9).times(function (i) {
-                return this.$grid.find("td").filter(function (j, cell) {
+                return this.$grid.find(".cell").filter(function (j, cell) {
                     return $(cell).index() === i;
                 });
             }, this);
@@ -27,7 +27,7 @@ $(document).ready(function () {
             this.boxes = _.flatten(_(3).times(function (i) {
                 var $band = this.$grid.find("tr").slice(3 * i, 3 * (i + 1));
                 return _(3).times(function (j) {
-                    return $band.find("td").filter(function (k, cell) {
+                    return $band.find(".cell").filter(function (k, cell) {
                         var index = $(cell).index();
                         return index >= 3 * j && index < 3 * (j + 1);
                     });
@@ -47,8 +47,9 @@ $(document).ready(function () {
             _(9).times(function () {
                 var $row = $("<tr />").appendTo($grid);
                 _(9).times(function () {
-                    var $cell = $("<td><input type='text' maxlength='5' pattern='[0-9]*' /></td>")
-                        .appendTo($row);
+                    var $cell = $("<td class='cell'>" +
+                            "<input type='text' maxlength='5' pattern='[0-9]*' /></td>")
+                            .appendTo($row);
                 });
             });
 
@@ -56,13 +57,13 @@ $(document).ready(function () {
         },
 
         populateGrid: function () {
-            this.$grid.find("td input").val("").removeAttr("readonly");
+            this.$grid.find(".cell input").val("").removeAttr("readonly");
             var givenCount = 0;
             while (givenCount < 20) {
                 var rowIndex = _.random(0, 8),
                     columnIndex = _.random(0, 8),
                     digit = _.random(1, 9),
-                    $input = this.$grid.find("tr").eq(rowIndex).find("td").eq(columnIndex)
+                    $input = this.$grid.find("tr").eq(rowIndex).find(".cell").eq(columnIndex)
                         .find("input");
                 if ($input.val() === "" && this.moveIsValid(rowIndex, columnIndex, digit)) {
                     $input.val(digit).attr("readonly", true)
@@ -75,32 +76,32 @@ $(document).ready(function () {
             var plugin = this,
                 $grid = plugin.$grid;
 
-            $grid.on("keypress", "td input", function (event) {
+            $grid.on("keypress", ".cell input", function (event) {
                 var rowIndex = $(this).closest("tr").index(),
-                    columnIndex = $(this).closest("td").index(),
+                    columnIndex = $(this).closest(".cell").index(),
                     $targetCell,
                     value = $(this).val();
                 if (event.keyCode === 37 && columnIndex > 0) {  // left
                     var cursorIsAtStart = this.selectionEnd === 0;
                     if (value.length === 0 || cursorIsAtStart || $(this).is("[readonly]")) {
-                        $targetCell = $(this).closest("tr").find("td").eq(columnIndex - 1);
+                        $targetCell = $(this).closest("tr").find(".cell").eq(columnIndex - 1);
                     }
                 } else if (event.keyCode === 38 && rowIndex > 0) {  // up
-                    $targetCell = $grid.find("tr").eq(rowIndex - 1).find("td").eq(columnIndex);
+                    $targetCell = $grid.find("tr").eq(rowIndex - 1).find(".cell").eq(columnIndex);
                 } else if (event.keyCode === 39 && columnIndex < 8) {  // right
                     var cursorIsAtEnd = this.selectionStart === value.length;
                     if (value.length === 0 || cursorIsAtEnd || $(this).is("[readonly]")) {
-                        $targetCell = $(this).closest("tr").find("td").eq(columnIndex + 1);
+                        $targetCell = $(this).closest("tr").find(".cell").eq(columnIndex + 1);
                     }
                 } else if (event.keyCode === 40 && rowIndex < 8) {  // down
-                    $targetCell = $grid.find("tr").eq(rowIndex + 1).find("td").eq(columnIndex);
+                    $targetCell = $grid.find("tr").eq(rowIndex + 1).find(".cell").eq(columnIndex);
                 }
                 if ($targetCell) {
                     $targetCell.find("input").select();
                 }
             });
 
-            $grid.on("input", "td input", function (event) {
+            $grid.on("input", ".cell input", function (event) {
                 var fontEms = ([1, 1, 0.9, 0.6, 0.5, 0.4][$(this).val().length] || 0.4);
                 $(this).css("font-size", fontEms + "em");
                 plugin.updateConflicts();
@@ -143,7 +144,7 @@ $(document).ready(function () {
             if (digit === "") {
                 return true;
             } else if (/^[1-9]$/.test(digit)) {
-                var $cell = this.$grid.find("tr").eq(rowIndex).find("td").eq(columnIndex),
+                var $cell = this.$grid.find("tr").eq(rowIndex).find(".cell").eq(columnIndex),
                     $row = _(this.rows).find(function ($r) {
                         return $r.is($cell);
                     }),
@@ -165,11 +166,11 @@ $(document).ready(function () {
         updateConflicts: function () {
             var $conflicts = this.findConflicts();
             $conflicts.addClass("conflict");
-            this.$grid.find("td").not($conflicts).removeClass("conflict");
+            this.$grid.find(".cell").not($conflicts).removeClass("conflict");
         },
 
         isWin: function () {
-            return _(this.getValues(this.$grid.find("td"))).all(function (value) {
+            return _(this.getValues(this.$grid.find(".cell"))).all(function (value) {
                 return /^[1-9]$/.test(value);
             }) && this.findConflicts().length === 0;
         },
